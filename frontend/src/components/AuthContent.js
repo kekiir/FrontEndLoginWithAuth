813e52e419b920b2853be56b6deae5a221e20424
Kéki Szilveszter <kekiir@gmail.com>
2024-10-02 00:31:28 +0200

    Authenticated /supplier/capacity endpoint is reached, the respons data is shown.

--- frontend/src/components/AuthContent.js
+++ frontend/src/components/AuthContent.js
@@ -17,7 +17,7 @@ export default class AuthContent extends React.Component {
             "/supplier/capacity",
             {}).then(
             (response) => {
-                this.setState({data: response.data})
+                this.setState({data: response.data.capacityResponseListDTO})
             }).catch(
             (error) => {
                 if (error.response.status === 401) {
@@ -31,24 +31,34 @@ export default class AuthContent extends React.Component {
     };
 
   render() {
-    return (
-        <div className="row justify-content-md-center">
-            <div className="col-4">
-                <div className="card" style={{width: "18rem"}}>
-                    <div className="card-body">
-                        <h5 className="card-title">Backend response</h5>
-                        <p className="card-text">Content:</p>
-                        <ul>
-                            {this.state.data && this.state.data
-                                .map((line) =>
-                                    <li key={line}>{line}</li>
-                                )
-                            }
-                        </ul>
+     return (
+            <div className="row justify-content-md-center">
+                <div className="col-4">
+                    <div className="card" style={{ width: "18rem" }}>
+                        <div className="card-body">
+                            <h5 className="card-title">Backend response</h5>
+                            <p className="card-text">Content:</p>
+                            <ul>
+                                {this.state.data && this.state.data.length > 0 ? (
+                                    this.state.data.map((line) => (
+                                        <li key={line.id}>
+                                            <strong>ID:</strong> {line.id} <br />
+                                            <strong>Energy Source:</strong> {line.energySource} <br />
+                                            <strong>Amount MW:</strong> {line.amountMW} <br />
+                                            <strong>Available MW:</strong> {line.available} <br />
+                                            <strong>Price:</strong> {line.price}$ <br />
+                                            <strong>From:</strong> {new Date(line.fromTime).toLocaleString()} <br />
+                                            <strong>To:</strong> {new Date(line.toTime).toLocaleString()} <br />
+                                        </li>
+                                    ))
+                                ) : (
+                                    <li>No data available</li>
+                                )}
+                            </ul>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
-    );
+        );
   };
 }
